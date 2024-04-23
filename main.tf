506de53b4db8480670fb1ae34efa998c60894f4e
CodingManoj <CodingManoj@gmail.com>
2024-04-22 21:44:38 -0400

    Auto Push

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 module "vpc" {
-  source = "./vendor/modules/vpc/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
+    source              = "./vendor/modules/vpc/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
+    VPC_CIDR            = var.VPC_CIDR
+    PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
+    PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+
 }
