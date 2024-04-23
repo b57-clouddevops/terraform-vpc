b2632aed65f42139d9d593f21e34b7d6a1a30d44
CodingManoj <CodingManoj@gmail.com>
2024-04-22 21:51:29 -0400

    Auto Push

--- main.tf
+++ main.tf
@@ -3,5 +3,7 @@ module "vpc" {
     VPC_CIDR            = var.VPC_CIDR
     PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
     PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-
 }
+
+# We cannot parametrize anything that's added n the source.
+# To limit that, we can use a tool called as Terrafile. All it does is clonnes the remote code from the specified branch and make it locally available.
