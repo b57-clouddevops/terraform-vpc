2d2f82192a69862af9d53e7af1d8573f859f7cbc
CodingManoj <CodingManoj@gmail.com>
2024-04-29 21:30:26 -0400

    Auto Push

--- main.tf
+++ main.tf
@@ -1,14 +1,21 @@
 module "vpc" {
-    source              = "./vendor/modules/vpc/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
-    VPC_CIDR            = var.VPC_CIDR
-    PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
-    PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-    ENV                 = var.ENV
-    AZ                  = var.AZ
-    DEFAULT_VPC_ID      = var.DEFAULT_VPC_ID
-    DEFAULT_VPC_CIDR    = var.DEFAULT_VPC_CIDR
-    DEFAULT_VPC_RT      = var.DEFAULT_VPC_RT
+    source                  = "./vendor/modules/vpc/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
+    VPC_CIDR                = var.VPC_CIDR
+    PUBLIC_SUBNET_CIDR      = var.PUBLIC_SUBNET_CIDR
+    PRIVATE_SUBNET_CIDR     = var.PRIVATE_SUBNET_CIDR
+    ENV                     = var.ENV
+    AZ                      = var.AZ
+    DEFAULT_VPC_ID          = var.DEFAULT_VPC_ID
+    DEFAULT_VPC_CIDR        = var.DEFAULT_VPC_CIDR
+    DEFAULT_VPC_RT          = var.DEFAULT_VPC_RT
+    PUBLIC_HOSTEDZONE_ID    = var.PUBLIC_HOSTEDZONE_ID
+    PUBLIC_HOSTEDZONE_NAME  = var.PUBLIC_HOSTEDZONE_NAME
+    PRIVATE_HOSTEDZONE_ID   = var.PRIVATE_HOSTEDZONE_ID
+    PRIVATE_HOSTEDZONE_NAME = var.PRIVATE_HOSTEDZONE_NAME
 }
 
 # We cannot parametrize anything that's added n the source.
 # To limit that, we can use a tool called as Terrafile. All it does is clonnes the remote code from the specified branch and make it locally available.
+
+
+
---
+++ terraform.auto.tfvars
@@ -0,0 +1,5 @@
+# Declare the values that are common to all the environments or use this to consider these as varaibles that should not be over-riden
+PUBLIC_HOSTEDZONE_ID    = "Z03169043MR7GRPXZ59K1"
+PUBLIC_HOSTEDZONE_NAME  = "b56-roboshop.online"
+PRIVATE_HOSTEDZONE_ID   = "Z08185973US3IG8LL97B8"
+PRIVATE_HOSTEDZONE_NAME = "roboshop.internal"
